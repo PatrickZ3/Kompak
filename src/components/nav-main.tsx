96bfc06e480eb3e6616851a591da9f419f0e07e1
Patrick <68784059+PatrickZ3@users.noreply.github.com>
2025-03-25 16:12:18 -0400

    side bar created

---
+++ src/components/nav-main.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { IconCirclePlusFilled, IconCirclePlus, IconMail, type Icon } from "@tabler/icons-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+export function NavMain({
+  items,
+}: {
+  items: {
+    title: string
+    url: string
+    icon?: Icon
+  }[]
+}) {
+  return (
+    <SidebarGroup className="pt-0">
+      <SidebarGroupContent className="flex flex-col">
+        <SidebarMenu>
+          <div className="pt-2 pb-2">
+            <SidebarMenuButton
+              tooltip="Quick Create"
+              className="min-w-8 bg-primary text-primary-foreground duration-200 ease-linear hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground"
+            >
+              <IconCirclePlus className="mr-2 !h-5 !w-5" />
+              <span>New Story</span>
+            </SidebarMenuButton>
+          </div>
+
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton className="text-sm font-medium" tooltip={item.title}>
+                {item.icon && <item.icon className="mr-2 !h-5 !w-5" />}
+                <span>{item.title}</span>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  )
+}
