5b8a48266800892bd9911bd71961ac020dd19ce1
Patrick <68784059+PatrickZ3@users.noreply.github.com>
2025-03-28 13:47:22 -0400

    Update story-cards.tsx

--- src/components/story-cards.tsx
+++ src/components/story-cards.tsx
@@ -1,7 +1,8 @@
 "use client"
 import React from 'react'
-import { IconTrendingDown, IconTrendingUp } from "@tabler/icons-react"
-
+import {
+    IconDotsVertical,
+} from "@tabler/icons-react"
 import { Badge } from "@/components/ui/badge"
 import { Prioritybadge } from './ui/priority-badge'
 import {
@@ -12,6 +13,12 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 import { Avatar, AvatarImage, AvatarFallback } from './ui/avatar'
 import { useDraggable } from "@dnd-kit/core";
@@ -75,34 +82,47 @@ export default function StoryCards({ stories }: StoryCardsProps) {
         <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:@xl/main:grid-cols-2 @5xl/main:grid-cols-4">
             {stories.map((story) => (
                 <DraggableCard key={story.id} story={story}>
-                <Card className="@container/card cursor-pointer" data-slot="card" key={story.id}>
-                    <CardHeader>
-                        <CardTitle className="text-lg font-bold tabular-nums">
-                            {story.story}
-                        </CardTitle>
-                    </CardHeader>
+                    <Card className="@container/card relative" data-slot="card" key={story.id}>
+                        <CardHeader>
+                            <CardTitle className="text-lg font-bold tabular-nums">
+                                {story.story}
+                            </CardTitle>
 
-                    <div className="flex items-center px-6">
-                        <Badge variant="default">{story.storyID}</Badge>
-                        <CardDescription className="pl-2 text-xs">
-                            {formatTimeLabel(story)}
-                        </CardDescription>
-                    </div>
-
-                    <CardDescription className="px-6 h-8 truncate-2-lines">{story.description}</CardDescription>
-
-                    <CardFooter className="flex-col items-start gap-1.5 text-sm">
-                        <div className="text-muted-foreground flex items-center justify-between w-full">
-                            <Prioritybadge variant={story.priority.toLowerCase() as "low" | "medium" | "high"}>{story.priority}</Prioritybadge>
-                            <Avatar className="h-8 w-8 rounded-lg">
-                                <AvatarImage src={story.user.avatar} alt={story.user.name} />
-                                <AvatarFallback className="rounded-2xl bg-primary text-primary-foreground">
-                                    CN
-                                </AvatarFallback>
-                            </Avatar>
+                        </CardHeader>
+                        <div className="absolute right-4 top-4 ">
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                    <button className="text-muted-foreground hover:text-foreground transition cursor-pointer">
+                                        <IconDotsVertical className="w-4 h-4" />
+                                    </button>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent align="end" className="w-32">
+                                    <DropdownMenuItem>Edit</DropdownMenuItem>
+                                    <DropdownMenuItem>Delete</DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
                         </div>
-                    </CardFooter>
-                </Card>
+                        <div className="flex items-center px-6">
+                            <Badge variant="default">{story.storyID}</Badge>
+                            <CardDescription className="pl-2 text-xs">
+                                {formatTimeLabel(story)}
+                            </CardDescription>
+                        </div>
+
+                        <CardDescription className="px-6 h-8 truncate-2-lines">{story.description}</CardDescription>
+
+                        <CardFooter className="flex-col items-start gap-1.5 text-sm">
+                            <div className="text-muted-foreground flex items-center justify-between w-full">
+                                <Prioritybadge variant={story.priority.toLowerCase() as "low" | "medium" | "high"}>{story.priority}</Prioritybadge>
+                                <Avatar className="h-8 w-8 rounded-lg">
+                                    <AvatarImage src={story.user.avatar} alt={story.user.name} />
+                                    <AvatarFallback className="rounded-2xl bg-primary text-primary-foreground">
+                                        CN
+                                    </AvatarFallback>
+                                </Avatar>
+                            </div>
+                        </CardFooter>
+                    </Card>
                 </DraggableCard>
             ))}
         </div>
