eb2499c8e0987bf9de2d70d326978db58f1ecfef
Patrick <68784059+PatrickZ3@users.noreply.github.com>
2025-03-30 19:45:04 -0400

    comit

--- src/components/progress-board.tsx
+++ src/components/progress-board.tsx
@@ -54,7 +54,7 @@ export default function ProgressBoard({ columns, userStories }: ProgressBoardPro
           <DroppableColumn
             key={colTitle}
             id={colTitle}
-            className="bg-sidebar rounded px-4 py-2 w-full min-h-[88vh]"
+            className="bg-sidebar rounded px-4 py-2 w-full min-h-[92vh]"
           >
             <div className="flex items-center justify-between w-full mb-2">
               <h2 className="text-secondary-foreground font-bold">{colTitle}</h2>
